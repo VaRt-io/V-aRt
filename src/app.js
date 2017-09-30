4e5b57de8ebbc2ceec3cde32eee52c30f824b5ff
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-09-30 16:55:14 -0400

    created a s3 blob service

--- src/app.js
+++ src/app.js
@@ -6,12 +6,15 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 
 // feathers-blob service
-const blobService = require('feathers-blob');
+const {getBase64DataURI} = require('dauria');
+const AWS = require('aws-sdk');
+const S3BlobStore = require('s3-blob-store');
+const BlobService = require('feathers-blob');
 // Here we initialize a FileSystem storage,
 // but you can use feathers-blob with any other
 // storage service like AWS or Google Drive.
-const fs = require('fs-blob-store');
-const blobStorage = fs(__dirname + '/uploads');
+// const fs = require('fs-blob-store');
+// const blobStorage = fs(__dirname + '/uploads');
 
 
 const feathers = require('feathers');
@@ -59,7 +62,42 @@ app.configure(services);
 
 
 // Upload Service
-app.use('/uploads', blobService({Model: blobStorage}));
+// app.use('/uploads', BlobService({Model: blobStorage}));
+
+// S3 Image Service
+const s3 = new AWS.S3({
+  accessKeyId: 'AKIAI4NXXMVNEKPISIMQ',
+  secretAccessKey: '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'
+})
+
+const blobStore = S3BlobStore({
+  client: s3,
+  bucket: 'stanky-clams'
+});
+
+const blobService = BlobService({
+  Model: blobStore
+});
+// TODO: programatically create blob and store galleryId & position as key-values pairs
+app.get('/s3/images', (req, res, next) => {
+  const blob = {
+    uri: getBase64DataURI(new Buffer('hello world'), 'text/plain')
+  }
+
+  blobService.create(blob).then(function (result) {
+    console.log('Stored blob with result:**', result);
+    res.json(result);
+  }).catch(err => {
+    console.error(err);
+  });
+})
+
+// TODO: programatically get all images
+
+
+// TODO: programatically get all images from a specific gallery
+
+
 
 // Configure a middleware for 404s and the error handler
 app.use(notFound());
