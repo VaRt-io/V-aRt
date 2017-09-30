6e063698b6143252ca0dbd540727f0a7fb225181
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-09-30 15:59:03 -0400

    removed image service, model, and associations. removed extraneous folder

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,7 @@
-const images = require('./images/images.service.js');
 const users = require('./users/users.service.js');
 const galleries = require('./galleries/galleries.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
-  app.configure(images);
   app.configure(users);
   app.configure(galleries);
 };
