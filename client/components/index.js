4a23433ce7afc2f537350671d0c40c21b42be025
Jason Rosso <jasonrosso@gmail.com>
2017-10-02 18:36:53 -0400

    some frontend

---
+++ client/components/index.js
@@ -0,0 +1,10 @@
+
+
+
+export {default as LandingPage} from './landingPage'
+export{default as Canvas} from './canvas';
+export{default as OurNavbar} from './navbar'
+export{default as OurCarousel} from'./landingPageFeatured'
+export{default as AllArtists} from './allArtists'
+export{default as SingleArtist} from './singleArtist'
+export{ default as AllGalleries} from './allGalleries'
