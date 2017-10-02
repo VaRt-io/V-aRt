86918a527c199bf9110739bb833ae153e820edfa
Tawsif Ahmed <Sif@Tawsifs-MacBook-Pro.local>
2017-10-02 19:11:23 -0400

    fixed undefined hook data for the s3/images/new endpoint

--- src/app.js
+++ src/app.js
@@ -167,16 +167,20 @@ app.service('/s3/images/new').before({
   create: [
     function(hook) {
       if (!hook.data.uri && hook.params.file){
+        console.log('got here');
         const file = hook.params.file;
         const uri = dauria.getBase64DataURI(file.buffer, file.mimetype);
         hook.data = {uri: uri};
       }
+      console.log('does this run?', hook.data.test);
+      const galleryid = hook.data.galleryid ? hook.data.galleryid.toString() : '0';
+      const position = hook.data.position ? hook.data.position.toString() : '0'
       hook.params.s3 = {
         ACL: 'public-read',
         Key: hook.data.name,
         Metadata: {
-          'galleryid': hook.data.galleryId.toString(),
-          'position': hook.data.position.toString()
+          'galleryid': galleryid,
+          'position': position
         }
       };
       // hook.params.name = hook.data.name;
