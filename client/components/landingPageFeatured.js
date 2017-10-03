233ac28baec79058b63651abb7cdb824ea902f15
Jason <Jason@Jasons-MacBook-Pro-2.local>
2017-10-03 15:59:54 -0400

    beta oyster front end

--- client/components/landingPageFeatured.js
+++ client/components/landingPageFeatured.js
@@ -4,7 +4,9 @@ import {Carousel} from 'react-bootstrap'
 export default class ourCarousel extends Component{
     
         render(){
+      
             return(
+              <div>
                 <Carousel className="col-md-6 col-lg-6 col-xs-12">
                 <Carousel.Item>
                   <img className='carImg' width={500} height={500} alt="900x500" src="https://www.bloomberg.com/features/2017-art-auction-estimates-quiz/img/art-quiz-bloomberg-question-02.jpg"/>
@@ -14,20 +16,21 @@ export default class ourCarousel extends Component{
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img width={500} height={500} alt="900x500" src="https://www.bloomberg.com/features/2017-art-auction-estimates-quiz/img/art-quiz-bloomberg-question-02.jpg"/>
+                  <img className='carImg' width={500} height={500} alt="900x500" src="https://www.bloomberg.com/features/2017-art-auction-estimates-quiz/img/art-quiz-bloomberg-question-02.jpg"/>
                   <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img width={500} height={500} alt="900x500" src="https://www.bloomberg.com/features/2017-art-auction-estimates-quiz/img/art-quiz-bloomberg-question-02.jpg"/>
+                  <img className='carImg' width={500} height={500} alt="900x500" src="https://www.bloomberg.com/features/2017-art-auction-estimates-quiz/img/art-quiz-bloomberg-question-02.jpg"/>
                   <Carousel.Caption>
                     <h3>Third slide label</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                   </Carousel.Caption>
                 </Carousel.Item>
               </Carousel>
+              </div>
             )
         }
     }
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
+import {Link} from 'react-router-dom';
+import {LinkContainer} from 'react-router-bootstrap'
 
 
 export default class OurNavbar extends Component{
 
     render(){
         return(
-            <Navbar inverse collapseOnSelect>
+            <Navbar id="ourNavbar"inverse collapseOnSelect>
             <Navbar.Header>
               <Navbar.Brand>
-                <a href="#">VaRt</a>
+                <LinkContainer to='/'><a href="#">VaRt</a></LinkContainer>
               </Navbar.Brand>
               <Navbar.Toggle />
             </Navbar.Header>
             <Navbar.Collapse>
               <Nav>
-                <NavItem eventKey={1} href="">Galleries</NavItem>
-                <NavItem eventKey={2} href="#">Artists</NavItem>
+                <LinkContainer to='/galleries'><NavItem eventKey={1} href="">Galleries</NavItem></LinkContainer>
+                <LinkContainer to='/artists'><NavItem eventKey={2} href="">Artists</NavItem></LinkContainer>
                 <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
                   <MenuItem eventKey={3.1}>Action</MenuItem>
                   <MenuItem eventKey={3.2}>Another action</MenuItem>
@@ -26,8 +28,8 @@ export default class OurNavbar extends Component{
                 </NavDropdown>
               </Nav>
               <Nav pullRight>
-                <NavItem eventKey={1} href="#">Link Right</NavItem>
-                <NavItem eventKey={2} href="#">Link Right</NavItem>
+                <NavItem eventKey={1} href="#">Sign In</NavItem>
+                
               </Nav>
             </Navbar.Collapse>
           </Navbar>
