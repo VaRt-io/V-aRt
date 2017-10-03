e1ec768f34506dc4eebdfc37de9f3e56bf20bf56
Jason Rosso <jasonrosso@gmail.com>
2017-10-03 15:59:54 -0400

    beta oyster front end

---
+++ client/components/artists-single.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { NavLink} from 'react-router-dom';
+
+
+export default class SingleArtist extends Component{
+
+    render(){
+        return(
+            <h1>singleArtists </h1>
+        )
+    }
+
+}
