68a7f5ee821552923228b3f706bef1c4ebcfb185
Tawsif Ahmed <ahmedt93@gmail.com>
2017-10-03 14:19:19 -0400

    implemented find for images service

--- src/services/images/images.class.js
+++ src/services/images/images.class.js
@@ -16,57 +16,55 @@ class Service {
   }
 
   find (params) {
-    // console.log(params);
-    // const s3Params = {Bucket: 'stanky-clams'};
-    //
-    // var resultantArr = [];
-    // var objectKeys = [];
-    // // Fetch list of objects in bucket
-    // // Make an http request for the head of each object
-    // // If there is a galleryid query param
-    // // filter the head objects by the specified gallery id
-    // // Construct objects consisting of the galleryid, position, urls joining the listObjects with the ObjectHeads on the last modified value
-    // // Else do the same as the if statement, but without filtering by the gallery id
-    //
-    // s3.listObjectsAsync(s3Params)
-    //   .then((data) => {
-    //     objectKeys = data.Contents;
-    //     return data.Contents.map((object) => s3.headObjectAsync({Bucket: 'stanky-clams', Key: object.Key}));
-    //   })
-    //   .then((allObjectHeadPromises) => Promise.all(allObjectHeadPromises))
-    //   .then((allObjectHeads) => {
-    //     if (req.query.hasOwnProperty('galleryid')) {
-    //       return allObjectHeads.filter((objectHead) => {
-    //         if (objectHead.Metadata.hasOwnProperty('galleryid') && objectHead.Metadata.hasOwnProperty('position')) {
-    //           return objectHead.Metadata.galleryid === req.query.galleryid;
-    //         }
-    //       });
-    //     } else {
-    //       allObjectHeads.forEach((headObject) => {
-    //         objectKeys.forEach((objectKey) => {
-    //           if (headObject.LastModified.toString() === objectKey.LastModified.toString()) {
-    //             resultantArr.push({url:`https://s3.amazonaws.com/stanky-clams/${objectKey.Key}`, position: headObject.Metadata.position, galleryid: headObject.Metadata.galleryid});
-    //           }
-    //         });
-    //       });
-    //       res.json(resultantArr);
-    //     }
-    //   })
-    //   .then((filteredObjects) => {
-    //     filteredObjects.forEach((filteredObject) => {
-    //       objectKeys.forEach((objectKey) => {
-    //         if (filteredObject.LastModified.toString() === objectKey.LastModified.toString()) {
-    //           resultantArr.push({url:`https://s3.amazonaws.com/stanky-clams/${objectKey.Key}`, position: filteredObject.Metadata.position, galleryid: filteredObject.Metadata.galleryid});
-    //         }
-    //       });
-    //     });
-    //     res.json(resultantArr);
-    //   })
-    //   .catch((err) => {
-    //     if (err.message !== 'Cannot read property \'forEach\' of undefined') {
-    //       console.log(err);
-    //     }
-    //   });
+    const s3Params = {Bucket: 'stanky-clams'};
+    var resultantArr = [];
+    var objectKeys = [];
+    // Fetch list of objects in bucket
+    // Make an http request for the head of each object
+    // If there is a galleryid query param
+    // filter the head objects by the specified gallery id
+    // Construct objects consisting of the galleryid, position, urls joining the listObjects with the ObjectHeads on the last modified value
+    // Else do the same as the if statement, but without filtering by the gallery id
+
+    return s3.listObjectsAsync(s3Params)
+      .then((data) => {
+        objectKeys = data.Contents;
+        return data.Contents.map((object) => s3.headObjectAsync({Bucket: 'stanky-clams', Key: object.Key}));
+      })
+      .then((allObjectHeadPromises) => Promise.all(allObjectHeadPromises))
+      .then((allObjectHeads) => {
+        if (params.query.hasOwnProperty('galleryid')) {
+          return allObjectHeads.filter((objectHead) => {
+            if (objectHead.Metadata.hasOwnProperty('galleryid') && objectHead.Metadata.hasOwnProperty('position')) {
+              return objectHead.Metadata.galleryid === params.query.galleryid;
+            }
+          });
+        } else {
+          allObjectHeads.forEach((headObject) => {
+            objectKeys.forEach((objectKey) => {
+              if (headObject.LastModified.toString() === objectKey.LastModified.toString()) {
+                resultantArr.push({url:`https://s3.amazonaws.com/stanky-clams/${objectKey.Key}`, position: headObject.Metadata.position, galleryid: headObject.Metadata.galleryid});
+              }
+            });
+          });
+          return resultantArr;
+        }
+      })
+      .then((filteredObjects) => {
+        filteredObjects.forEach((filteredObject) => {
+          objectKeys.forEach((objectKey) => {
+            if (filteredObject.LastModified.toString() === objectKey.LastModified.toString()) {
+              resultantArr.push({url:`https://s3.amazonaws.com/stanky-clams/${objectKey.Key}`, position: filteredObject.Metadata.position, galleryid: filteredObject.Metadata.galleryid});
+            }
+          });
+        });
+        return resultantArr;
+      })
+      .catch((err) => {
+        if (err.message !== 'Cannot read property \'forEach\' of undefined') {
+          console.log(err);
+        }
+      });
   }
 
   get (id, params) {
