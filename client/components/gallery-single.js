fc763918ab74ffb2400dfdfa2e1f5656b5aa41e1
Jason Rosso <jasonrosso@gmail.com>
2017-10-04 14:20:10 -0400

    wednesday afternoon merge

--- client/components/gallery-single.js
+++ client/components/gallery-single.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { NavLink, Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import store from '../store'
 
-
-export default class SingleGallery extends Component{
+class SingleGallery extends Component{
 
     render(){
+        console.log("SINGL GALLERY PROPS",this.props)
+        const currentGalleryId =this.props.match.params.id
+        console.log(currentGalleryId)
+        const galleries = this.props.galleryCollection
+        const currentGallery = galleries.length &&galleries.filter(gallery=>+gallery.id===+currentGalleryId)[0]
+
+        console.log("CURRENT GALLERY",currentGallery)
         return(
             <div>
-            <h1>Gallery Name</h1>
+            <h1>{currentGallery.name}</h1>
                 <Link to='/vr'><button className="btn btn-danger" style={{backgroundColor:"red"}}>Enter VR</button></Link>
                 <h3>map over images</h3>
                     <a href="link to canvas"><button className="btn btn-success">Create an Image</button></a>
@@ -16,3 +24,11 @@ export default class SingleGallery extends Component{
     }
 
 }
+
+const mapState =(state,ownProps)=>{
+    return{
+        galleryCollection: state.galleries.galleryCollection,
+    }
+}
+
+export default connect(mapState)(SingleGallery)
