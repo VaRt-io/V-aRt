70c35806fc5401cca044fdd9a88344e4cce8fa08
Alex Press <press.alex@gmail.com>
2017-10-04 16:58:08 -0400

    all artists rendering

--- client/components/artists-all.js
+++ client/components/artists-all.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
-import { NavLink} from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+
+import store, {fetchUsers} from '../store';
 
 
-export default class AllArtists extends Component{
+class AllArtists extends Component {
+
+  componentDidMount(){
+    console.log('artist comp MOUNTED');
+        console.log(fetchUsers)
+    const action = fetchUsers();
+    store.dispatch(action);
+  }
 
   render(){
+    console.log('PROPPPPSSSS', this.props);
+    const artists = this.props.artistCollection;
+    console.log('ARTistS', artists);
     return (
+      <div id="allGalleryBox">
       <h1>allArtists </h1>
+        <div id="allGalleryRender">
+          {
+            artists && artists.map(artist => {
+              return (
+                <div key={artist.id}>
+                  <h3 >
+                    <Link to= {`/artists/${artist.id}`}>{artist.email}</Link>
+                  </h3>
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
     );
   }
 
 }
+
+const mapState = (state, ownProps) => {
+  return {
+    artistCollection: state.users.artistCollection
+  };
+};
+
+export default connect(mapState)(AllArtists);
