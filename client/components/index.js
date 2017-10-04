be84f28668b3f1dd7fb8bf1d54f88483f8495910
Jason Rosso <jasonrosso@gmail.com>
2017-10-04 18:28:45 -0400

    singleArtist before major backend merge

--- client/components/index.js
+++ client/components/index.js
@@ -1,7 +1,7 @@
 
 
 export {default as LandingPage} from './landingPage';
-export {default as Canvas} from './canvas';
+
 export {default as OurNavbar} from './navbar';
 export {default as OurCarousel} from './landingPageFeatured';
 export {default as AllArtists} from './artists-all';
