cf0fcee94d5072ef44b6bcd1aee079cc9450077b
Tawsif Ahmed <Sif@Tawsifs-MacBook-Pro.local>
2017-10-04 13:09:42 -0400

    updated eslint settings for team

--- client/components/Camera.js
+++ client/components/Camera.js
@@ -1,11 +1,10 @@
-import 'aframe'
-import 'aframe-animation-component'
-import {Entity, Scene} from 'aframe-react';
-import {Component}from 'react';
-import ReactDOM from 'react-dom'
+import 'aframe';
+import 'aframe-animation-component';
+import { Entity, Scene } from 'aframe-react';
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-function Camera(){
-    return(
-        
-    )
+function Camera() {
+  return ( <div > camera </div>
+  );
 }
