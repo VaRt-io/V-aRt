1ad1af8c3c84df48b42c3d4218fc768ff8a711a8
Tawsif Ahmed <ahmedt93@gmail.com>
2017-10-04 12:56:58 -0400

    changed linter to fullstack linter

--- client/components/galleries-all.js
+++ client/components/galleries-all.js
@@ -9,9 +9,12 @@ export default class AllGalleries extends Component{
       <div>
         <h1>allGalleries </h1>
 
+
+
         <Link to="galleries/1">Gallery 1</Link>
       </div>
     );
   }
+
 
 }
