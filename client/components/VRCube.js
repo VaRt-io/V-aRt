a8e0b314ca3fcacbcc796ceaf56fee353b075a96
Jason Rosso <jasonrosso@gmail.com>
2017-10-07 18:26:31 -0400

    patched image service so it posts to the paintings table

--- client/components/VRCube.js
+++ client/components/VRCube.js
@@ -7,10 +7,15 @@ import ReactDOM from 'react-dom';
 export default class VR extends Component{
 
   render(){
+    // THIS IS THE PIECE OF CODE ACCESSING THE PAINTINGS ARRAY WE PASSED IN FROM SINGLE GALLERY VIEW 
+    const paintingsArray= this.props.location.fromSingleGallery.paintings;
     console.log('VR PROPS', this.props);
+    console.log('PAINTINGS ARRAY', paintingsArray);
+    console.log("Paintings arrayy 0 URL", paintingsArray[0].url);
+    //THIS IS THE PIECE OF CODE ACCESSING THE PAINTINGS ARRAY WE PASSED IN FORM SINGLE GALLERY VIEW
     return (
       <Scene>
-        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}} />
+        <Entity geometry={{primitive: 'box'}} material={{src: paintingsArray[0].url}} position={{x: 0, y: 0, z: -5}} />
         <Entity particle-system={{preset: 'snow'}} />
         <Entity light={{type: 'point'}} />
         <Entity gltf-model={{src: 'virtualcity.gltf'}} />
