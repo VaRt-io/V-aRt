eed7b1153878b3d829bb0cebe5bec605bc7900ac
James Dooley <dbnserial@gmail.com>
2017-10-07 16:23:45 -0400

    added dynamic routing for VR component

--- client/components/index.js
+++ client/components/index.js
@@ -12,3 +12,4 @@ export {default as CreateGallery} from './createGallery';
 export {default as VR} from './VR';
 export {default as VR2} from './VR2';
 export {default as CanvasWrapper} from './CanvasWrapper';
+export {default as VRWrapper} from './VRWrapper'
