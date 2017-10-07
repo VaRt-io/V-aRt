857fb45ad02da0c0581fcf5d2328ad46bd85a9ce
Jason Rosso <jasonrosso@gmail.com>
2017-10-07 18:26:31 -0400

    patched image service so it posts to the paintings table

--- client/components/main.js
+++ client/components/main.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-
-import {LandingPage,  OurNavbar, AllArtists, SingleArtist, AllGalleries, SingleGallery, VR, VR2, CanvasWrapper, GalleryCreate, SinglePainting, GalleryEdit, Signup} from './index';
-
+import {LandingPage,  OurNavbar, AllArtists, SingleArtist, AllGalleries, SingleGallery, VRNightScene, VRCube, CanvasWrapper, GalleryCreate, SinglePainting, GalleryEdit} from './index';
 import AframeVR from '../containers/wrapper';
 
 import store, {getGalleriesThunk, fetchUsers, getPaintingsThunk} from '../store';
@@ -26,19 +24,18 @@ export default class Main extends Component{
         <OurNavbar />
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route exact path="/vr/:galleryId" component={AframeVR} />
           <Route exact path="/artists/:id" component={SingleArtist} />
           <Route exact path="/artists" component={AllArtists} />
           <Route exact path="/galleries" component={AllGalleries} />
           <Route exact path="/galleries/:id" component={SingleGallery} />
+          <Route exact path="/galleries/:id/vr/nightscene" component={VRNightScene} />
           <Route exact path="/gallery-create" component={GalleryCreate} />
-          <Route exact path="/gallery-edit" component={GalleryEdit} />
-          <Route exact path="/vr" component={VR} />
-          <Route exact path="/vr2" component={VR2} />
+          <Route exact path="/gallery-edit/:id" component={GalleryEdit} />
+          <Route exact path="/vr/cube" component={VRCube} />
+          <Route exact path="/vr/nightscene" component={VRNightScene} />
+          <Route exact path="/vr/:galleryId" component={AframeVR} />
           <Route exact path="/canvas" component={CanvasWrapper} />
           <Route exact path="/paintings/:id" component={SinglePainting} />
-
-          <Route exact path="/signup" component={Signup} />
 
         </Switch>
       </div>
