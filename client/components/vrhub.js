fd35fc52bab6af994ab497a233e843fa6b6e1f5f
James Dooley <dbnserial@gmail.com>
2017-10-09 14:02:45 -0400

    add vr hubz

--- client/components/vrhub.js
+++ client/components/vrhub.js
@@ -6,20 +6,16 @@ import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import store from '../store';
-// import AllImages from '../VRComponents/AllImages.js'
-
-const defaultGallery = {id:99, paintings: [{url: "https://blogs.ancestry.com/ancestry/files/2014/09/Brickwall.jpg"}]}
 
 export function VRHub (props){
             var galleries = props.galleries;
             var positions = ['-1.50 2.0 -9.65', '3.454 2.00 -9.65', '5.000 2.00 -6.20' , '5.000 2.00 -2.00', '2.50 2.0 2.10', '-1.50 2.0 2.10', '-4.00 2.0 -1.50', '-4.00 2.0 -7.00']
             var rotations = ['0 0 0', '0 0 0', '0 -90 0', '0 -90 0', '0 180 0', '0 180 0', '0 90 0', '0 90 0'];
             return(
-            <Scene physics="friction: 0.1; restitution: 0.5">
-                <a-assets>
+            <a-scene physics="friction: 0.1; restitution: 0.5">
+                 <a-assets>
                     <a-asset-item id="frame-obj" src="/models/Frame/Old_picture_frame.obj"></a-asset-item>
                     <a-asset-item id="frame-mtl" src="/models/Frame/Old_picture_frame.mtl"></a-asset-item>
-
                 </a-assets>
 
                 <a-plane height="20" width="20" src="https://blogs.ancestry.com/ancestry/files/2014/09/Brickwall.jpg" position="-4.17 7.831 -1.71" rotation="0 90 0"></a-plane>
@@ -28,16 +24,27 @@ export function VRHub (props){
                 <a-plane height="20" width="20" src="https://blogs.ancestry.com/ancestry/files/2014/09/Brickwall.jpg" position="4.729 8.618 2.151" rotation="0 180 0"></a-plane>
                 <a-plane color="#CCC" height="20" width="20" position="-2.89 0.047 -4.31" rotation="-90 0 0"></a-plane>
                 <a-plane color="#CCC" height="20" width="20" position="5.680 7.015 0.019" rotation="90 0 0"></a-plane>
+ 
+                <a-entity obj-model="obj:#frame-obj" material="color:blue" position="1 2.759 -9.52" rotation="90 0 0" href={`/galleries`}>
+                    <a-image src='https://blogs.ancestry.com/ancestry/files/2014/09/Brickwall.jpg.thumbnailUrl' position="0 .4 0" scale="1 .7 1"></a-image>
+                </a-entity>
+                
+                <a-entity obj-model="obj:#frame-obj" material="color:red" position=".537 3.138 1.285" rotation="-90 0 0" href="/vr/hub">
+                    <a-image src='https://blogs.ancestry.com/ancestry/files/2014/09/Brickwall.jpg' position="0 .4 0" scale="1 .7 1"></a-image>
+                </a-entity>
 
-                { galleries && galleries.map((image, index) => {
-
+                { galleries && galleries.map((gallery, index) => {
+                    console.log(gallery.thumbnailUrl)
                     return(
-                    <a-entity key={index} obj-model="obj:#frame-obj" material= "color:blue" position={positions[index]} rotation={rotations[index]} scale="2.5 2.5 1">
-                        <a-image src={galleries.url} position="0 .4 0" scale="1 .7 1"></a-image>
+                    <a-entity key={gallery.id} obj-model="obj:#frame-obj" material= "color:blue" position={positions[index]} rotation={rotations[index]} scale="2.5 2.5 1" href={`/galleries/${gallery.id}`}>
+                        <a-image src={gallery.thumbnailUrl} position="0 .4 0" scale="1 .7 1"></a-image>
                     </a-entity>
                 )})
                 }
-            </Scene>
+                <a-entity camera="userHeight: 1.6" look-controls wasd-controls>
+                    <a-entity id="cursor" position="0 0 -2" cursor geometry="primitive: ring; radiusOuter: 0.11; radiusInner: 0.08" material="color: white"></a-entity>
+                </a-entity>
+            </a-scene>
        );
        
 }
