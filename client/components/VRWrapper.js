63aab39d0101ab2b9f638998fcf7bb6aa81cb95d
James Dooley <dbnserial@gmail.com>
2017-10-09 16:08:48 -0400

    testing physics

--- client/components/VRWrapper.js
+++ client/components/VRWrapper.js
@@ -1,6 +1,6 @@
 import 'aframe';
 import 'aframe-particle-system-component';
-import 'aframe-physics-system'
+import 'aframe-physics-components'
 import 'aframe-href-component'
 import {Entity, Scene} from 'aframe-react';
 import React,{Component} from 'react';
@@ -18,7 +18,7 @@ export function AframeVR (props){
         var positions = ['-1.50 2.0 -9.65', '3.454 2.00 -9.65', '5.000 2.00 -6.20' , '5.000 2.00 -2.00', '3.15 2.0 2.10', '-2.10 2.0 2.10', '-4.00 2.0 -1.50', '-4.00 2.0 -7.00']
         var rotations = ['0 0 0', '0 0 0', '0 -90 0', '0 -90 0', '0 180 0', '0 180 0', '0 90 0', '0 90 0'];
             return(
-            <a-scene physics="friction: 0.1; restitution: 0.5">
+            <a-scene physics-world="gravity: 0 -9.8 0">
                 <a-assets>
                     <a-asset-item id="frame-obj" src="/models/Frame/Old_picture_frame.obj"></a-asset-item>
                     <a-asset-item id="frame-mtl" src="/models/Frame/Old_picture_frame.mtl"></a-asset-item>
@@ -26,7 +26,7 @@ export function AframeVR (props){
                     <a-asset-item id="button-mtl" src="/models/Button/button.mtl"></a-asset-item>
                 </a-assets>
 
-                <a-plane height="20" width="20" src="https://blogs.ancestry.com/ancestry/files/2014/09/Brickwall.jpg" position="-4.17 7.831 -1.71" rotation="0 90 0"></a-plane>
+                <a-plane physics-body="boundingBox: 1 1 1" height="20" width="20" src="https://blogs.ancestry.com/ancestry/files/2014/09/Brickwall.jpg" position="-4.17 7.831 -1.71" rotation="0 90 0"></a-plane>
                 <a-plane height="20" width="20" src="https://blogs.ancestry.com/ancestry/files/2014/09/Brickwall.jpg" position="0.573 7.741 -9.667" rotation="0 0 0"></a-plane>
                 <a-plane height="20" width="20" src="https://blogs.ancestry.com/ancestry/files/2014/09/Brickwall.jpg" position="5.157 8.266 0.114" rotation="0 -90 0"></a-plane>
                 <a-plane height="20" width="20" src="https://blogs.ancestry.com/ancestry/files/2014/09/Brickwall.jpg" position="4.729 8.618 2.151" rotation="0 180 0"></a-plane>
@@ -49,7 +49,7 @@ export function AframeVR (props){
                     </a-entity>
                 )})
                 }
-                <a-entity camera="userHeight: 1.6" look-controls wasd-controls>
+                <a-entity physics-body="boundingBox: 1 1 1" camera="userHeight: 1.6" look-controls wasd-controls>
                     <a-entity id="cursor" position="0 0 -2" cursor geometry="primitive: ring; radiusOuter: 0.11; radiusInner: 0.08" material="color: white"></a-entity>
                 </a-entity>
             </a-scene>
