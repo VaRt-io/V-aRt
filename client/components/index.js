c27811880e82f18f29c298f915b50cb1d30720ca
James Dooley <dbnserial@gmail.com>
2017-10-09 14:16:09 -0400

    vrhub and dynamic routing

--- client/components/index.js
+++ client/components/index.js
@@ -1,5 +1,4 @@
 
-
 export {default as LandingPage} from './landingPage';
 
 export {default as OurNavbar} from './navbar';
@@ -10,12 +9,18 @@ export {default as AllGalleries} from './galleries-all';
 export {default as SingleGallery} from './gallery-single';
 export {default as GalleryCreate} from './gallery-create';
 export {default as GalleryEdit} from './gallery-edit';
-export {default as VR} from './VR';
-export {default as VR2} from './VR2';
+export {default as VRCube} from './VRCube';
+export {default as VRNightScene} from './VRNightScene';
 export {default as CanvasWrapper} from './CanvasWrapper';
 export {default as VRWrapper} from './VRWrapper'
+export {default as VRHub} from './vrhub'
 export {default as SinglePainting} from './singlePainting';
 export {default as Signup} from './signup';
 export {default as Signin} from './signin';
 export {default as ArtistDashboard} from './artist-dashboard';
-export {default as VRHub} from './vrhub'
+export {default as DisplayPaintings} from './display-paintings';
+export {default as OurPageHeader} from './display-ourPageHeader';
+export {default as OurJumbotron} from './display-ourJumbotron';
+export {default as DisplaySelectFromFS} from './display-selectFromFS';
+export {default as DisplaySelectForm} from './display-selectForm';
+export {default as PaintingDropdown} from  './display-paintingDropdown';
