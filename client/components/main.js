87e7cc3a48fd471651e735979fe7f96a46cd3581
Jason Rosso <jasonrosso@gmail.com>
2017-10-10 10:33:59 -0400

    jason tuesday morning commit

--- client/components/main.js
+++ client/components/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-import {LandingPage,  OurNavbar, AllArtists, SingleArtist, AllGalleries, SingleGallery, VR, VR2, CanvasWrapper, GalleryCreate, SinglePainting, GalleryEdit, Signup, Signin, ArtistDashboard, VRWrapper, VRNightScene, VRCube, VRHub} from './index';
+import {LandingPage,  OurNavbar, AllArtists, SingleArtist, AllGalleries, SingleGallery, VR, VR2, CanvasWrapper, GalleryCreate, SinglePainting, GalleryEdit, Signup, Signin, ArtistDashboard, VRWrapper, VRNightScene, VRCube} from './index';
 
 
 // import AframeVR from '../containers/wrapper';
@@ -25,23 +25,21 @@ export default class Main extends Component{
 
   render(){
     return (
+    
       <div id="landingBackground">
         <OurNavbar />
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route exact path="/vr/hub" component={VRHub} />
-          <Route exact path="/vr/:galleryId/:environment" component={VRWrapper} />
           <Route exact path="/artists/:id" component={SingleArtist} />
           <Route exact path="/artists" component={AllArtists} />
           <Route exact path="/galleries" component={AllGalleries} />
           <Route exact path="/galleries/:id" component={SingleGallery} />
-          <Route exact path="/galleries/:id/vr/nightscene" component={VRNightScene} />
           <Route exact path="/gallery-create" component={GalleryCreate} />
           <Route exact path="/gallery-edit/:id" component={GalleryEdit} />
-          <Route exact path="/vr/cube" component={VRCube} />
-          <Route exact path="/vr/nightscene" component={VRNightScene} />
-          <Route exact path="/vr/:galleryId" component={VRWrapper} />
           <Route exact path="/canvas" component={CanvasWrapper} />
+          <Route exact path="/vr/nightscene" component={VRNightScene} />
+          <Route exact path="/vr/:id/nightscene" component={VRNightScene} />
+          <Route exact path="/vr/:id/cube" component={VRCube} />    
           <Route exact path="/paintings/:id" component={SinglePainting} />
           <Route exact path="/dashboard" component={ArtistDashboard} />
           <Route exact path="/signup" component={Signup} />
