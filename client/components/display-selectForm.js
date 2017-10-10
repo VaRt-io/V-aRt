a6914782409eb0ed25076e03476facc6836b31d2
Jason <Jason@Jasons-MacBook-Pro-2.local>
2017-10-10 13:59:56 -0400

    desert with dali clock

--- client/components/display-selectForm.js
+++ client/components/display-selectForm.js
@@ -5,10 +5,15 @@ import React,{Component} from 'react';
 export default function DisplaySelectFromFS (props){
     // console.log("DiSPLaY fRoM Fs PrOps",props);
     let handleChange= props.handleChange;
+    let selected = props.selected;
+    
+    
     return(
         <select name="environment" onChange={handleChange}>
+            <option>Selected-{selected}</option>
             <option  value="nightscene">nightscene</option>
             <option  value="cube">cube</option>
+            <option value="desert">desert</option>
         </select>
         );
 
