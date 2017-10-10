416e7bf18a395345cb79dc8a86dbb0e953f921ea
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-10-10 18:01:41 -0400

    pre multi env refactor

---
+++ client/components/VRCity.js
@@ -0,0 +1,46 @@
+import 'aframe';
+import 'aframe-particle-system-component';
+import {Entity, Scene} from 'aframe-react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+function VRCity(props){
+        const currentGalleryId = props.match.params.id;
+        const galleries = props.galleriesCollection;
+        const currentGallery = galleries.length && galleries.filter(gallery => +gallery.id === +currentGalleryId)[0];
+        let paintings;   //current galleries will be async, we have to wait for it to come in before we can define paintings 
+        if(currentGallery){
+            paintings= currentGallery.paintings;
+        }
+        // DEFINE ALL YOUR IMAGE TEXTURES HERE AND SAVE THEM AS CONSTANTS 
+        // EX.) const marbleTexture = 'https://ucarecdn.com/1b213dc4-386d-4978-8fe5-9b021b23c945/';
+        // YOU WILL BE USING THEM ON YOU OBJECTS BELOW
+        
+        return(
+            paintings?
+            <Scene>
+            <a-entity environment="preset: forest"></a-entity>
+          </Scene>
+            :
+            null
+        )
+    
+}
+
+const mapState = function(state){
+    return {
+        galleriesCollection: state.galleries.galleryCollection,
+    }
+};
+
+export default connect(mapState)(VRCity);
+
+/*
+           <Scene>
+            <a-assets>
+            <a-asset-item id="cityModel" src="https://cdn.aframe.io/test-models/models/virtualcity/VC.gltf"></a-asset-item>
+            </a-assets>
+
+            <a-gltf-model src="#cityModel"></a-gltf-model>
+            </Scene>
+*/
