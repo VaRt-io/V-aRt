e1d7dc8ea8345645ed2e95ca9f92cb368b446c67
Tawsif Ahmed <Sif@Tawsifs-MacBook-Pro.local>
2017-10-11 12:39:33 -0400

    added conditional rendering for starry

--- client/components/VRStarry.js
+++ client/components/VRStarry.js
@@ -16,41 +16,58 @@ function VRStarry(props){
         return(
             paintings?
             <Scene>
-                <a-curvedimage 
-                    position="0 3 -16" 
-                    rotation="0 -27 0"
-                    height="3.0"
-                    radius="4.0"
-                    theta-length="60" 
-                    src={paintings[0].url}>
-                </a-curvedimage>
+            {
+                paintings.length >= 1 && 
+            <a-curvedimage 
+                position="0 3 -16" 
+                rotation="0 -27 0"
+                height="3.0"
+                radius="4.0"
+                theta-length="60" 
+                src={paintings[0].url}>
+            </a-curvedimage>
+            }
+            {
+                paintings.length >= 2 && 
+            <a-curvedimage 
+                position="16 3 0" 
+                rotation="0 240 0"
+                height="3.0"
+                radius="4.0"
+                theta-length="60" 
+                src={paintings[1].url}>
+            </a-curvedimage>  
+            }
+            {
+                paintings.length >= 3 && 
+            <a-curvedimage 
+                position="0 3 16" 
+                rotation="0 150 0"
+                height="3.0"
+                radius="4.0"
+                theta-length="60" 
+                src={paintings[2].url}>
+            </a-curvedimage>
+            }
 
-                <a-curvedimage 
-                    position="16 3 0" 
-                    rotation="0 240 0"
-                    height="3.0"
-                    radius="4.0"
-                    theta-length="60" 
-                    src={paintings[1].url}>
-                </a-curvedimage>          
-            
-                <a-curvedimage 
-                    position="0 3 16" 
-                    rotation="0 150 0"
-                    height="3.0"
-                    radius="4.0"
-                    theta-length="60" 
-                    src={paintings[2].url}>
-                </a-curvedimage>
-
-                <a-curvedimage 
+            {
+                paintings.length >= 4 &&
+            <a-curvedimage 
                 position="-15 3 3" 
                 rotation="0 60 0"
                 height="3.0"
                 radius="4.0"
                 theta-length="60" 
                 src={paintings[3].url}>
-            </a-curvedimage>  
+            </a-curvedimage> 
+            }
+
+
+        
+            
+
+
+ 
 
             <a-entity environment="preset: starry"></a-entity>            
           </Scene>
