a859ecb3e63bc296f1adb40a1ea6a5564bb2f2f2
Alex Press <press.alex@gmail.com>
2017-10-11 15:19:50 -0400

    minor style changes and removal of dead code

--- client/components/artists-all.js
+++ client/components/artists-all.js
@@ -9,33 +9,24 @@ class AllArtists extends Component {
 
 
   render(){
-    // console.log('PROPPPPSSSS', this.props);
     const artists = this.props.artistsCollection;
-    // console.log('ARTistS', artists);
     return (
       <div id="allGalleryBox">
-      <h1>allArtists </h1>
+      <h1>Artists</h1>
         <div id="allGalleryRender">
           {
             artists && artists.map(artist => {
-              // return (
-              //   <div key={artist.id}>
-              //     <h3 >
-              //       <Link to= {`/artists/${artist.id}`}>{artist.email}</Link>
-              //     </h3>
-              //   </div>
-              // );
               return (
               <div className="renderedBox col-md-3" key={artist.id}>
                 <div className="innerRenderedBox">
                   <img id="allGalleryThumb" src={artist.profileImageUrl} />
                   <div style={{marginTop: '15px'}}>
                     <Link id="allGalleriesText" to= {`/artists/${artist.id}`}>{artist.name}</Link>
-                    
+
                   </div>
 
               </div>
-              
+
             </div>
               );
             })
