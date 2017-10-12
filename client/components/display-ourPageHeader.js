c5f1f22ab5a13f9abe4dc97196639b59e97aa5b0
Alex Press <press.alex@gmail.com>
2017-10-12 15:19:39 -0400

    minor display changes

--- client/components/display-ourPageHeader.js
+++ client/components/display-ourPageHeader.js
@@ -3,17 +3,17 @@ import {Link} from 'react-router-dom';
 import {PageHeader} from 'react-bootstrap';
 
 export default function OurPageHeader (props){
-    // console.log("displayPaintings PrOps",props)
-    const currentGallery= props.currentGallery;
+    const currentGallery = props.currentGallery;
     const artistName = props.artistName;
     const artistId = props.artistId;
-    return(
-        <PageHeader>{currentGallery.title}
-        <small style={{color:'blue'}}> By: 
+
+    return (
+      <PageHeader>{currentGallery.title} &nbsp;
+        <small style={{color: 'blue'}}> By: &nbsp;
         <Link to={`/artists/${artistId}`}>{`${artistName}`}</Link>
         </small>
       </PageHeader>
-       
+
         );
 
 }
