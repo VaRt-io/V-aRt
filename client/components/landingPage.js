31bad7b3795c2ca6fa10be12c6232a7329ac6741
Jason <Jason@Jasons-MacBook-Pro-2.local>
2017-10-12 22:03:10 -0400

    new landing page

--- client/components/landingPage.js
+++ client/components/landingPage.js
@@ -9,13 +9,34 @@ export default class Main extends Component{
   render(){
     return (
           <div >
-            <div>
+            <div id="landingPageHeader" style={{marginLeft:'27vw'}}>
               <h3 style={{margin: '30px 0px 0px 30px'}}>V-aRt is  a virtual art experience,</h3>
               <h3 style={{margin: '0px 0px 0px 50px'}}>merging the technology of virtual reality</h3>
               <h3 style={{margin: '0px 0px 0px 70px'}}>with the talent of artists arcoss the Globe.</h3>
               <h3 style={{margin: '0px 0px 50px 90px'}}>Enjoy browsing our virtual galleries and museums.</h3>
+    
             </div>
             <OurCarousel />
+
+           
+
+            <div id="landingPageDescriptors">
+            <div className="col-md-4">
+            <h3>Paint <span class="glyphicon glyphicon-pencil"></span></h3>
+            <h3>Create beautiful masterpieces on our canvas or drag and drop your own images</h3>
+            </div>
+            <div className="col-md-4">
+            <h3>Browse<span class="glyphicon glyphicon-bookmark"></span></h3>
+            <h3>Discover and follow new artists and browse through their galleries for inspiration</h3>
+            </div>
+            <div className="col-md-4">
+            <h3>View In VR <span class="glyphicon glyphicon-sunglasses"></span></h3>
+            <h3>See artworks come to life in surrealistic three dimensional environments</h3>
+            </div>
+            </div>
+
+            <div className="col-md-12"></div>
+
            </div>
     );
   }
