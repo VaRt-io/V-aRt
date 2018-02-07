dfd85a939e5764c3ab634ec893d7afc709c8832e
Jason Rosso <jasonrosso@gmail.com>
2018-02-07 11:51:30 -0500

    password check added to signup

--- client/components/signup.js
+++ client/components/signup.js
@@ -9,10 +9,13 @@ class Signup extends Component {
     this.state = {
       name: '',
       email: '',
-      password: ''
+      password: '',
+      passwordCheck:'',
+      warning: false
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.passwordCheckAndSubmit=this.passwordCheckAndSubmit.bind(this);
   }
 
   handleChange(event){
@@ -22,12 +25,25 @@ class Signup extends Component {
       [event.target.name]: value
     });
   }
+  passwordCheckAndSubmit(evt){
+    evt.preventDefault();
+    console.log('password',this.state.password);
+    console.log('passwordCheck',this.state.passwordCheck);
+    if(this.state.password !== this.state.passwordCheck){
+    this.setState({warning: true});
+    }else {
+      this.props.handleSubmit(evt,this.props.history);
+    }
+  }
 
   render(){
     const handleChange = this.handleChange;
 
       return (
-        <Form horizontal className="formBoxLogin" onSubmit={(evt) => this.props.handleSubmit(evt, this.props.history)}>
+        <Form horizontal className="formBoxLogin" onSubmit={
+          // (evt) => this.props.handleSubmit(evt, this.props.history)
+          (evt)=>this.passwordCheckAndSubmit(evt)
+        }>
 
         <FormGroup controlId="formHorizontalName">
           <Col componentClass={ControlLabel} sm={3}>
@@ -73,6 +89,23 @@ class Signup extends Component {
               style={{backgroundColor: 'grey', color: 'lightgreen'}} />
           </Col>
         </FormGroup>
+
+        <FormGroup controlId="formHorizontalPasswordCheck">
+        <Col componentClass={ControlLabel} sm={3}>
+          Re-Enter Password
+        </Col>
+        <Col sm={9}>
+          <FormControl
+            type="password"
+            name="passwordCheck"
+            value={this.state.passwordCheck}
+            placeholder="Re-enter Password"
+            onChange={handleChange}
+            style={{backgroundColor: 'grey', color: 'lightgreen'}} />
+        </Col>
+      </FormGroup>
+
+        {this.state.warning ? <h5 style={{color:'red'}}>Passwords do not match</h5>:null}
 
         <FormGroup>
           <Col smOffset={2} sm={9}>
