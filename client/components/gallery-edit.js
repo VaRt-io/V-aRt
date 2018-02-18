de51bfe436fcd8345adbdc187cadf52c37d8daf8
Jason Rosso <jasonrosso@gmail.com>
2018-02-18 18:58:29 -0500

    responsive edit gallery and new button

--- client/components/gallery-edit.js
+++ client/components/gallery-edit.js
@@ -24,6 +24,7 @@ class GalleryEdit extends Component{
             environment,
             thumbnailUrl,
             environmentPic,
+            mqMatch: null, //jsut a dummy state to force rerender
 
         };
         this.handleChange = this.handleChange.bind(this);
@@ -42,6 +43,7 @@ class GalleryEdit extends Component{
             environment,
             thumbnailUrl,
             environmentPic,
+            
         });
         }
     }
@@ -77,7 +79,20 @@ class GalleryEdit extends Component{
           artistName = currentGallery.user.name;
           artistId = currentGallery.user.id;
         }
-
+        const mq = window.matchMedia('(min-width:992px)');
+        mq.addListener((changed)=>{
+            if(changed.matches){
+              console.log('BIG SCREEN!!!!!');
+              console.log(mq);
+              this.setState({mqMatch: true});
+            }
+            else{
+              this.setState({mqMatch: false});
+            }
+          });
+        const mqMatch= this.state.mqMatch;
+        console.log(mq)
+        console.log(currentGallery);
         return (
 
         <div className="editGalleryContainter">
@@ -89,42 +104,55 @@ class GalleryEdit extends Component{
 
             <div className="galleryCover">
 
-                <div className="innerGalleryCover">
+                <div className="innerGalleryCover clearfix">
 
-                    <div className="editGalleryColumn">
-                        <h3>Gallery Cover Image</h3>
+                    <div className="editGalleryColumn  col-xs-6 col-md-4 col-lg-4" id='coverPhoto'>
+                        <h4>Gallery Cover Image</h4>
                         <img
-                          className="galleryCover"
+                          className="editGalImg"
                           src={this.state.thumbnailUrl}
-                          style={{height: '300px', width: '400px'}} />
+                           />
                     </div>
 
-                    <div className="editGalleryColumn">
-                        <h3 >Gallery Cover</h3>
+                    <div className="editGalleryColumn  col-xs-6 col-md-2 col-lg-2" id='selectGalCov'>
+                        <h4 >Cover Img</h4>
                         <PaintingDropdown currentGallery={currentGallery} handleChange={this.handleChange} selected={this.state.thumbnailUrl} />
                         <div id="enterVRButton" style={{textAlign: 'center', margin: 'auto'}}>
                         <Link to={`/vr/${currentGalleryId}/${this.state.environment}`}>
                         <button
-                          className="btn btn-danger"
-                          style={{backgroundColor: 'red', fontSize: '14px', margin: 'auto', color: '#222'}}>
+                          className="btn enterVartBtn"
+                          style={{fontSize: '14px', margin: 'auto', color: '#222'}}>
                         Enter VR</button>
                         </Link>
                       </div>
                     </div>
-
-                    <div className="editGalleryColumn">
-                         <h3>Gallery Environment</h3>
+                    { mq.matches ? 
+                    <div className="editGalleryColumn  col-xs-6 col-md-2 col-lg-2" id='selectGalEnv'>
+                         <h4>Gallery Env</h4>
                         <DisplaySelectForm handleChangeForEnvironment={this.handleChangeForEnvironment} selected={this.state.environment} />
                         <Button onClick={() => this.props.handleSubmit(this.state)} className="btn btn-success" style={{ height: '60px', margin: 'auto', color: '#101010'}}>Submit Changes</Button>
                     </div>
-
-                    <div className="editGalleryColumn">
-                    <h3>Environment</h3>
-                    <img
-                      className="galleryCover"
-                      src={this.state.environmentPic}
-                      style={{height: '300px', width: '400px'}} />
-                </div>
+                    :
+                    null
+                    }
+                    <div className="editGalleryColumn  col-xs-6 col-md-4 col-lg-4" id='envPhoto'>
+                        <h4>Environment</h4>
+                        <img
+                        className="editGalImg"
+                        src={this.state.environmentPic}
+                        />
+                    </div>
+                    {
+                        !mq.matches ? 
+                        <div className="editGalleryColumn  col-xs-6 col-md-2 col-lg-2" id='selectGalEnv'>
+                            <h4>Gallery Env</h4>
+                            <DisplaySelectForm handleChangeForEnvironment={this.handleChangeForEnvironment} selected={this.state.environment} />
+                            <Button onClick={() => this.props.handleSubmit(this.state)} className="btn btn-success" style={{ height: '60px', margin: 'auto', color: '#101010'}}>Submit Changes</Button>
+                        </div>
+                        :
+                        null
+                    }
+               
                 </div>
              </div>
 
